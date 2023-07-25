8c41da8be965a0e83ea9d980d4d10c444110e968
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-25 15:55:57 +0900

    Update 3moji.py

--- 3moji.py
+++ 3moji.py
@@ -4,6 +4,7 @@ from misskey import Misskey
 
 import random
 import os
+import time
 
 misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
 misskey_token = os.environ.get("MISSKEY_TOKEN")
@@ -14,9 +15,16 @@ moji3.token = misskey_token
 # coding:utf-8
 hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
 
-a3= random.choice(hiragana)
-b3= random.choice(hiragana)
-c3= random.choice(hiragana)
-
-moji3.notes_create(text=a3+b3+c3)
+try:
+  a3= random.choice(hiragana)
+  b3= random.choice(hiragana)
+  c3= random.choice(hiragana)
+  moji3.notes_create(text=a3+b3+c3)
+except:
+  time.sleep(30)
+  a3= random.choice(hiragana)
+  b3= random.choice(hiragana)
+  c3= random.choice(hiragana)
+  moji3.notes_create(text=a3+b3+c3)
+  
 print(a3+b3+c3+'\n')
